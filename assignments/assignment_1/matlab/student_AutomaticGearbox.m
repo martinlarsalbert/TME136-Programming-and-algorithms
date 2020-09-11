62ed86b13b53748234b6c3b88c3dbd7efdfc43ce
Martin Alexandersson <marale@kth.se>
2020-09-11 14:22:49 +0200

    Did my solution, I don't think it is the optimum, but I don't have the time to figure it out...

--- assignments/assignment_1/matlab/student_AutomaticGearbox.m
+++ assignments/assignment_1/matlab/student_AutomaticGearbox.m
@@ -3,12 +3,21 @@ function [gear_demand, Throttle] = student_AutomaticGearbox(Gear, RPM, LongAcc, 
 Speed = Velocity * 3.6;
 Throttle = 1;
 % Check the speed and determine gear
-if Speed < 30
-    gear_demand = 1;
-else 
-    gear_demand = 4;
-end
 
+if RPM > 9000
+    wanted_gear = Gear+1;
+else
+    wanted_gear = Gear;
+end;
+
+if wanted_gear==-1
+    wanted_gear=1;
+elseif wanted_gear>4;
+   wanted_gear=4;
+end;
+    
+gear_demand = wanted_gear;
+    
 % For comparasion, uncomment the line below to see how caster gearbox performs
 % gear_demand = caster_AutomaticGearbox(Gear, RPM, LongAcc, Velocity, Throttle, Distance, TimeLap);
 end
